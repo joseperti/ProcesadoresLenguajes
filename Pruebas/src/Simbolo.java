0a11b221ad8414ff565197c5aa3b1f5946f87c45
Sergio Santamaria <s.santamaria.most@gmail.com>
2016-05-05 18:27:52 +0200

    Añadida clase simbolo y empezamos a toquetear el cup pa añadir cosas

---
+++ Pruebas/src/Simbolo.java
@@ -0,0 +1,14 @@
+
+public class Simbolo {
+	private String op;
+	private Character tipoOperador;
+	private String tipoDato;
+	
+	private static final Character T_OP_LOG = 'l';
+	private static final Character T_OP_ARIT = 'a';
+	private static final Character T_OP_COMP = 'c';
+	private static final String T_DATO_INTEGER = "Integer";
+	private static final String T_DATO_REAL = "Real";
+	private static final String T_DATO_CHARACTER = "Character";
+	
+}
