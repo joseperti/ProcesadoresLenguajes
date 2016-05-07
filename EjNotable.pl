0d064202997a3d2707350b96836a22c1be2fd602
Joseperti <joseperti@users.noreply.github.com>
2016-05-07 02:55:45 +0200

    Proyecto avanzado y ejemplo.

---
+++ EjNotable.pl
@@ -0,0 +1,31 @@
+program EjemploNotable; 
+function areaCuadrado ( lado: REAL ) : REAL ;
+type miArray = array [ 0 .. 5 ] of INTEGER ;
+var
+	resultado: REAL;
+begin
+	resultado := +0.0;
+	miArray[1] := 0;
+	resultado := lado * lado + miArray[ 1 + 2 ];
+	areaCuadrado := resultado; 
+end; 
+procedure intercambio ( v1, v2: INTEGER )  ;
+var
+	aux: INTEGER;
+begin
+	if ( v1 <> v2 ) then 
+		aux := 0;
+	aux := v1;
+	v1 := v2;
+	v2 := aux;
+end;
+var
+	medida: REAL;
+	valor1, valor2: INTEGER;
+begin
+	medida := $4.A;
+	valor1 := -3;
+	valor2 := $F6;
+	while medida < valor1 do medida := areaCuadrado( medida );
+	intercambio(valor1, valor2);
+end.
